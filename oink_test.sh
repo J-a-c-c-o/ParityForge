12d48c65855368a145d03359f9826f4e8e35516f
Jacco te Poel <j.j.tepoel@student.utwente.nl>
2026-06-16 12:23:32 +0200

    moved oink away from the repo

--- oink_test.sh
+++ oink_test.sh
@@ -1,4 +1,4 @@
 mkdir -p tmp &&
 cargo build --release && 
 cd ./tmp && 
-../oink/build/test_solvers -e "../target/release/parityforge solve --algo fpj %I %O" -e "../target/release/parityforge solve --algo fpi %I %O" ../oink/tests/
+test_solvers -e "../target/release/parityforge solve --algo fpj %I %O" -e "../target/release/parityforge solve --algo fpi %I %O" ../oink/tests/
